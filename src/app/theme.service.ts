702b83daad03c5a493bf838631a01f60a2d2e277
LouziAmine <louzi.amine.pro@gmail.com>
2024-02-13 23:05:35 +0100

    dark light mode

--- src/app/theme.service.ts
+++ src/app/theme.service.ts
@@ -7,26 +7,59 @@ export class ThemeService {
 
  // constructor() { }
 
+  // setTheme(theme: string) {
+  //   if (typeof localStorage !== 'undefined') {
+  //     localStorage.setItem('theme', theme);
+  //     document.documentElement.className = theme;
+  //   }
+  // }
+  //
+  // getTheme() {
+  //   if (typeof localStorage !== 'undefined') {
+  //     return localStorage.getItem('theme') || 'light-mode';
+  //   }
+  //   return 'light-mode';
+  // }
+  //
+  // toggleTheme() {
+  //   if (typeof localStorage !== 'undefined') {
+  //     const currentTheme = this.getTheme();
+  //     const newTheme = currentTheme === 'light-mode' ? 'dark-mode' : 'light-mode';
+  //     this.setTheme(newTheme);
+  //     console.log(newTheme);
+  //   }
+  // }
+
+  private readonly localStorageAvailable: boolean;
+
+  constructor() {
+    this.localStorageAvailable = typeof window !== 'undefined' && window.localStorage != null;
+    // Initialize theme mode if localStorage is available
+    if (this.localStorageAvailable && !localStorage.getItem('theme')) {
+      // Set default theme mode to light-mode if not set yet
+      localStorage.setItem('theme', 'light-mode');
+    }
+  }
+
   setTheme(theme: string) {
-    if (typeof localStorage !== 'undefined') {
+    if (this.localStorageAvailable) {
       localStorage.setItem('theme', theme);
       document.documentElement.className = theme;
     }
   }
 
   getTheme() {
-    if (typeof localStorage !== 'undefined') {
+    if (this.localStorageAvailable) {
       return localStorage.getItem('theme') || 'light-mode';
     }
-    return 'light-mode';
+    return 'light-mode'; // Default to light mode if localStorage is not available
   }
 
   toggleTheme() {
-    if (typeof localStorage !== 'undefined') {
+    if (this.localStorageAvailable) {
       const currentTheme = this.getTheme();
       const newTheme = currentTheme === 'light-mode' ? 'dark-mode' : 'light-mode';
       this.setTheme(newTheme);
-      console.log(newTheme);
     }
   }
 }
